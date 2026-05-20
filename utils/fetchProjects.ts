dd63137e6cc68c80a02d4207fd822b5af211e0d6
Mel <121836871+melvinjones2@users.noreply.github.com>
2026-05-20 18:18:02 -0500

    feat: integrate Sanity CMS with Next.js
    - Added Sanity client configuration in `sanity.ts`.
    - Created API routes for fetching experiences, page info, projects, skills, and socials.
    - Defined TypeScript typings for Sanity data structures in `typings.d.ts`.
    - Implemented utility functions to fetch data from the newly created API endpoints.
    - Updated `package.json` to include necessary dependencies for Sanity integration.
    - Added an example environment file `.env.example` for configuration.

---
+++ utils/fetchProjects.ts
@@ -0,0 +1,12 @@
+import { Project } from "../pages/api/typings";
+
+export const fetchProjects = async() => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`);
+
+    const data = await res.json();
+    const projects: Project[] = data.projects;
+
+    //  console.log("fetchProjects: ", projects);
+    
+    return projects;
+}

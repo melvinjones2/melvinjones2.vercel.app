a8d7a13ba4e02d63e9379b74fc4c7e79f680a355
Mel <121836871+melvinjones2@users.noreply.github.com>
2026-05-29 13:47:16 -0500

    refactor About component layout for improved readability and consistency

--- components/About.tsx
+++ components/About.tsx
@@ -9,12 +9,12 @@ type Props = {
 export default function About({ pageInfo }: Props) {
   return (
     <motion.div
-        initial={{ opacity: 0, }}
-        whileInView={{ opacity: 1, }}
-        transition={{ duration: 1.5, }}
-        className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+      initial={{ opacity: 0, }}
+      whileInView={{ opacity: 1, }}
+      transition={{ duration: 1.5, }}
+      className="relative flex flex-col h-screen md:text-left md:flex-row max-w-7xl px-10 justify-center gap-y-6 md:gap-x-16 mx-auto items-center"
     >
-      <h3 className="absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-16 uppercase tracking-[20px] justify-center text-gray-500 text-2xl">
         About
       </h3>
 
@@ -24,24 +24,24 @@ export default function About({ pageInfo }: Props) {
           opacity: 0,
         }}
         transition={{
-            duration: 1.2,
+          duration: 1.2,
         }}
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src={urlFor(pageInfo?.profilePic).url()}
-        className="space-y-5 pb-0 md:pb-0 flex-shrink-0 w-50 h-50 rounded-full object-cover md:rounded-lg md:mt-0 sm:w-60 sm:h-60 md:w-64 md:h-95 xl:w-[400px] xl:h-[550px] justify-center"
+        className="flex-shrink-0 w-48 h-48 rounded-full object-cover sm:w-60 sm:h-60 md:w-64 md:h-80 xl:w-[350px] xl:h-[480px] md:rounded-lg"
       />
 
-      <div className="space-y-2 md:space-y-3 pt-5 px-5 md:px-10">
-        <h4 className="px-3 md:px-0 text-2xl md:text-4xl font-semibold">
-            Here is a {""} 
-            <span className="underline decoration-[#0a2af7]/50">little</span>{" "}
-            background
+      <div className="space-y-2 md:space-y-3 lg:px-10 text-center md:text-left">
+        <h4 className="text-2xl md:text-4xl font-semibold">
+          Here is a {""}
+          <span className="underline decoration-[#0a2af7]/50">little</span>{" "}
+          background
         </h4>
         {/* Add if long description is required...
         overflow-y-scroll scrollbar-hidden scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#0a2af7]/80 */}
-        <p className="text-md md:px-0 md:text-lg lg:text-xl leading-snug text-base tracking-tight md:tracking-normal overflow-y-auto scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#0a2af7]/80 max-h-70 sm:max-h-65 lg:max-h-70 xl:max-h-75">
-            {pageInfo?.backgroundInformation}
+        <p className="text-md md:text-lg lg:text-xl leading-snug text-base tracking-tight md:tracking-normal overflow-y-auto scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#0a2af7]/80 max-h-70 sm:max-h-65 lg:max-h-70 xl:max-h-75">
+          {pageInfo?.backgroundInformation}
         </p>
       </div>
     </motion.div>
